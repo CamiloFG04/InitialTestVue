46e538b67db6f115163681aa0e5c42a409d7156b
Camilo Fernandez <andrescamilo592@gmail.com>
2023-09-26 18:28:04 -0500

    Test in Components one simple and other with Fect

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <IndecisionVue/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import IndecisionVue from './components/Indecision.vue'
 export default {
+  components: { IndecisionVue },
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
